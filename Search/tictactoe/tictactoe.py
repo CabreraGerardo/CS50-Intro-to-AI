2527b5ce774d27ea6c589dbe4fb5e7433bfd60d9
Gerardo Cabrera <CabreraGerardo>
2024-07-07 01:12:59 -0600

    Implemented first tictactoe functions (player, actions, result, winner)

--- Search/tictactoe/tictactoe.py
+++ Search/tictactoe/tictactoe.py
@@ -22,35 +22,88 @@ def player(board):
     """
     Returns player who has the next turn on a board.
     """
-    raise NotImplementedError
+    lessRepeated = X;
+    for row in board:
+        xTimes = row.count(X)
+        yTimes = row.count(O)
 
+        if xTimes < yTimes or xTimes is yTimes:
+            lessRepeated = X
+        else:
+            lessRepeated = O
+
+    return lessRepeated
+        
 
 def actions(board):
     """
     Returns set of all possible actions (i, j) available on the board.
     """
-    raise NotImplementedError
+    actions = {}
+    for rowIdx, row in enumerate(board):
+       for colIdx, val in enumerate(row):
+           actions.add((rowIdx, colIdx))
+
+    return actions
 
 
 def result(board, action):
     """
     Returns the board that results from making move (i, j) on the board.
     """
-    raise NotImplementedError
+    try:
+        if board[action[0]][action[1]] is not EMPTY:
+            raise NameError("Action not valid")
+        
+        board[action[0]][action[1]] = player(board);
+    except:
+        raise NameError("Action not valid")
 
 
 def winner(board):
     """
     Returns the winner of the game, if there is one.
     """
-    raise NotImplementedError
+    players = [X, O]
 
+    winner = None
+
+    for player in players:
+        if checkHorizontaly(board, player) or checkVerticaly(board, player) or checkDiagonally(board, player)
+            winner = player
+    
+    return winner
+
+def checkHorizontaly(board, player):
+    for row in board:
+        if row.count(player) is 3:
+            return True;
+    
+def checkVerticaly(board, player):
+    for col in range(3):
+        if board[0][col] is not player:
+            return False
+        
+    return True
+
+def checkDiagonally(board, player):
+    colIndex = 0
+    for row in board:
+        if row[colIndex] is not player:
+            return False
+        colIndex += 1 
+
+    for row in board:
+        if row[colIndex] is not player:
+            return False
+        colIndex -= 1 
+    
+    return True
 
 def terminal(board):
     """
     Returns True if game is over, False otherwise.
     """
-    raise NotImplementedError
 
 
 def utility(board):
